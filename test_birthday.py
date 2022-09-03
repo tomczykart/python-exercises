2607af9bc1e74b5743e579605e73213e0ae044ad
tomczykart <tomczyk.art@gmail.com>
2022-09-03 20:04:30 +0200

    Added monkeypatch mock for input.

--- test_birthday.py
+++ test_birthday.py
@@ -1,7 +1,21 @@
-from birthday import calculate_when
+import birthday
+from io import StringIO
 
-def test_1_birthday():
-    assert calculate_when(22) == 2100
+def get_name():
+    name = input('Podaj swoje imie: ')
+    return name
 
-def test_2_birthday():
-    assert calculate_when(100) == 2022
+def test_get_name(monkeypatch):
+    user_input = StringIO('Artur\n')
+    monkeypatch.setattr('sys.stdin', user_input)
+    assert get_name() == 'Artur'
+
+def test_get_name_v2(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: 'Artur')
+    assert birthday.get_name() == 'Artur'
+
+#def test_1_birthday():
+#    assert calculate_when(22) == 2100
+
+#def test_2_birthday():
+#    assert calculate_when(100) == 2022
